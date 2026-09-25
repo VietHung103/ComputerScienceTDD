11a2221cb2d5dedc6a6d924f1ef8bd4cd4eab1bd
Viet Hung <viethung@Viets-MacBook-Pro.local>
2026-09-25 16:33:20 +0700

    Refractor Project

--- src/fizzbuzz_tdd_kata/__init__.py
+++ src/fizzbuzz_tdd_kata/__init__.py
@@ -1,6 +1,6 @@
-def main() -> None:
-    print("Hello from fizzbuzz-tdd-kata!")
-# from fizzbuzz_tdd_kata.core import fizzbuzz
+# def main() -> None:
+#     print("Hello from fizzbuzz-tdd-kata!")
+from fizzbuzz_tdd_kata.core import fizzbuzz as fizzbuzz
 
-# all = ["fizzbuzz"]
-# version=['0.1.0']
+all = ["fizzbuzz"]
+version=['0.1.0']
